a5f5bbcaa03a06a933ca405632094e69d2cfb2c6
Ahmed Tawfiq <ahmedadelmm26@gmail.com>
2021-12-26 18:42:19 +0200

    add AppNavigator

---
+++ lib/common/presentation/AppNavigator.dart
@@ -0,0 +1,15 @@
+import 'package:flutter/material.dart';
+
+void navigateByWidget(BuildContext context, Widget to) {
+  Navigator.of(context).push(MaterialPageRoute(builder: (context) => to));
+}
+
+void navigateByName(BuildContext context, String to) {
+  Navigator.of(context).pushNamed(to);
+}
+
+void navigateByReplacement(BuildContext context, Widget to) {
+  Navigator.of(context).pushReplacement(
+      MaterialPageRoute(builder: (context) => to)
+  );
+}
