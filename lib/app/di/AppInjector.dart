394b11c7918ee37c16ee10580611ddca67558086
Ahmed Tawfiq <ahmedadelmm26@gmail.com>
2022-01-09 21:24:04 +0200

    add AppInjector

---
+++ lib/app/di/AppInjector.dart
@@ -0,0 +1,14 @@
+//TODO add fun inject then add to Main
+
+class AppInjector {
+
+  blocs() {
+    //TODO: do stuff here
+    return [];
+  }
+
+  providers() {
+    //TODO: do stuff here
+    return [];
+  }
+}
--- lib/main.dart
+++ lib/main.dart
@@ -1,24 +1,31 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_bloc/flutter_bloc.dart';
+import 'package:provider/provider.dart';
 import 'package:starterflutter/app/AppStartUpFlow.dart';
+import 'package:starterflutter/app/di/AppInjector.dart';
 
 void main() {
-  runApp(const MyApp());
+  runApp(MyApp());
 }
 
 class MyApp extends StatelessWidget {
-  const MyApp({Key? key}) : super(key: key);
+  MyApp({Key? key}) : super(key: key);
+  final _appInjector = AppInjector();
 
   @override
   Widget build(BuildContext context) {
-    return MaterialApp(
-      title: 'Flutter Demo',
-      theme: ThemeData(
-
-        primarySwatch: Colors.blue,
+    return MultiBlocProvider(
+      providers: _appInjector.blocs(),
+      child: MultiProvider(
+        providers: _appInjector.providers(),
+        child: MaterialApp(
+          title: 'Starter Flutter',
+          theme: ThemeData(
+            primarySwatch: Colors.blue,
+          ),
+          home: AppStartUpFlow.mainScreen(),
+        ),
       ),
-      home: AppStartUpFlow.mainScreen(),
     );
   }
 }
-
-
