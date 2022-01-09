a7360a766c7e8036b7906e91f04abb3b1db2b603
Ahmed Tawfiq <ahmedadelmm26@gmail.com>
2022-01-09 21:51:57 +0200

    add App setup in main starter

--- lib/common/core/app_config.dart
+++ lib/common/core/app_config.dart
@@ -1,4 +1,4 @@
-class AppConfig {
+class AppConfig { //TODO: add your own Config :)
   static var apiKey = "c39edb1f487155d36e655629443197b7";
-  static var baseUrl = "https://RELATIVE_URL";
+  static var baseUrl = "https://RELATIVE_URL"; //TODO: add your own URL :)
 }
