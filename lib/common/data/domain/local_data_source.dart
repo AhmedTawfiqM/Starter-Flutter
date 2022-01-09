fe6f279ae3e70af9f7e9efc5656ca0b74d359690
Ahmed Tawfiq <ahmedadelmm26@gmail.com>
2022-01-09 21:46:40 +0200

    add samples of repo and viewmodel

---
+++ lib/common/data/domain/local_data_source.dart
@@ -0,0 +1,6 @@
+import 'package:starterflutter/common/data/domain/sample_web_service.dart';
+import 'package:starterflutter/common/data/shared/data_source_protocol.dart';
+
+class LocalDataSrc implements DataSourceProtocol {
+
+}
